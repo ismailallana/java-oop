bd4837e502e1923aef6bfce71db5769e372a3c27
Ismail Allana <ismailallana@gmail.com>
2021-06-10 04:56:26 +1000

    Add files via upload

---
+++ EmailService2020.java
@@ -0,0 +1,8 @@
+package com.mytube;
+
+public class EmailService2020 {
+    public void sendEmail(User user) {
+        System.out.println("Notifying " + user.getEmail() + "...");
+        System.out.println("Done!\n");
+    }
+}
